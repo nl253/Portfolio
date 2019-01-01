463d8c322365e2b529284e3d4470fd6b640b6fd1
Norbert Logiewa <norbertlogiewa96@gmail.com>
2019-01-01 16:18:53 +0000

    Initial

---
+++ ClientApp/src/components/Project.js
@@ -0,0 +1,110 @@
+import React, {Component} from 'react';
+import {Container} from 'reactstrap';
+
+export default class Project extends Component {
+
+  constructor(props) {
+    super(props);
+    if (props.imgs) this.state = {imgIdx: 0};
+  }
+
+  firstCell(text) {
+    return <td className="pr-3"><span
+        className="font-weight-bold text-capitalize">{text.toLowerCase()}</span>
+    </td>;
+  }
+
+  componentDidMount() {
+    if (!this.props.imgs) return;
+    this.setState({
+      interval: setInterval(() => {
+        return this.setState(
+            {imgIdx: (this.state.imgIdx + 1) % this.props.imgs.length});
+      }, 3000),
+    });
+  }
+
+  componentWillUnmount() {
+    if (this.state && this.state.interval) {
+      clearInterval(this.state.interval);
+    }
+  }
+
+  render() {
+    return (
+        <Container>
+          <h3 className="display-4">{this.props.name}</h3>
+          <table>
+            <tbody>
+            {this.props.date && (
+                <tr>
+                  {this.firstCell('date')}
+                  <td>
+                    {this.props.date}
+                  </td>
+                </tr>
+            )}
+            {this.props.language && (
+                <tr>
+                  {this.firstCell('language')}
+                  <td>
+                    {this.props.language.url &&
+                    <a href={this.props.language.url}>{this.props.language.name}</a> ||
+                    this.props.language}
+                  </td>
+                </tr>
+            )}
+            {this.props.framework && (
+                <tr>
+                  {this.firstCell('framework')}
+                  <td>{this.props.framework.url &&
+                  <a href={this.props.framework.url}>{this.props.framework.name}</a> ||
+                  this.props.framework}</td>
+                </tr>
+            )}
+            {this.props.frameworks && (
+                <tr>
+                  {this.firstCell('frameworks')}
+                  <td>
+                    {this.props.frameworks.map((f, idx, arr) => f.url
+                        ?
+                        <a href={f.url}>{f.name}{idx < (arr.length - 1) ?
+                            ', ' :
+                            ''}</a>
+                        :
+                        <span>{f}{idx < (arr.length - 1) ? ', ' : ''}</span>)}
+                  </td>
+                </tr>
+            )}
+            {this.props.methodology && (
+                <tr>
+                  {this.firstCell('methodology')}
+                  <td>{this.props.methodology}</td>
+                </tr>
+            )}
+            {this.props.repo && (
+                <tr>
+                  {this.firstCell('repository')}
+                  <td><a href={this.props.repo}>{this.props.repo}</a></td>
+                </tr>
+            )}
+            {this.props.links && (
+                <tr>
+                  {this.firstCell('other links')}
+                  <td>{this.props.links}</td>
+                </tr>
+            )}
+            </tbody>
+          </table>
+          {this.props.summary && <p className="mt-2">{this.props.summary}</p>}
+          {this.props.imgs && (
+              <div className="container-fluid">
+                {this.props.imgs.map(
+                    (img, idx, arr) => <img src={img.src} alt={img.alt}
+                                            className={(this.state.imgIdx === idx ? '' : 'd-none') + ' mw-100'}/>)}
+              </div>
+          )}
+        </Container>
+    );
+  }
+}
---
+++ ClientApp/src/index.js
@@ -0,0 +1,236 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import registerServiceWorker from './registerServiceWorker';
+import {Container, Navbar} from 'reactstrap';
+import Project from './components/Project';
+
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+
+ReactDOM.render(
+    <Router basename={baseUrl}>
+      <div>
+        <header>
+          <Navbar color="dark"
+                  className="navbar-expand-sm border-bottom box-shadow mb-3"
+                  dark>
+            <h1 className="text-white text-center w-100">Portfolio</h1>
+          </Navbar>
+        </header>
+        <Container>
+          <div className="container-fluid">
+            <div className="row mt-5">
+              <aside className="col-xl-2 col-lg-2 col-md-3 col-sm-12 pl-0 mb-4">
+                <img className="w-100 pb-2"
+                     src="https://lh3.googleusercontent.com/a-/AAuE7mBn6m7vkSG6jeniJfhzGwIdojv_2NntjcpH_nTIDw=s192"
+                     alt="selfie"/>
+                <p className="mb-4">
+                <span
+                    className="font-weight-bold d-block">Norbert Logiewa</span>
+                  <span className="font-italic mt-2 d-block">Computer Science with<br/>Artificial Intelligence</span>
+                  <a className="mt-2 d-block" href="https://www.kent.ac.uk">University
+                    of Kent</a>
+                </p>
+                <h2 className="text-center">Projects</h2>
+                <nav>
+                  <Link to="/projects/sqlite-repl"
+                        className="d-block mb-2 btn btn-info">
+                    SQLite REPL
+                  </Link>
+                  <Link to="/projects/flasky-blog"
+                        className="d-block mb-2 btn btn-info">
+                    Flasky Blog
+                  </Link>
+                  <Link to="/projects/virtual-machine"
+                        className="d-block mb-2 btn btn-info">
+                    Virtual Machine
+                  </Link>
+                  <Link to="/projects/regex-engine"
+                        className="d-block mb-2 btn btn-info">
+                    Regex Engine
+                  </Link>
+                  <Link to="/projects/free-learn"
+                        className="d-block mb-2 btn btn-info">
+                    Free Learn
+                  </Link>
+                  <Link to='/projects/taxi-cumulus'
+                        className="d-block mb-2 btn btn-info">
+                    Taxi Cumulus
+                  </Link>
+                </nav>
+              </aside>
+              <div className="col-xl-9 col-lg-9 col-md-8">
+                <Route path='/projects/free-learn'
+                       component={() => <Project name="Free Learn"
+                                                 methodology="Agile (Scrum)"
+                                                 repo="https://github.com/nl253/CO600_Project"
+                                                 date="October 2018 to May 2019"
+                                                 imgs={[
+                                                   {
+                                                     src: '/img/FreeLearnHome.jpg',
+                                                     alt: 'registration and login forms of a website',
+                                                   },
+                                                   {
+                                                     src: '/img/FreeLearnCreatorDashboard.jpg',
+                                                     alt: 'creator dashboard allowing to create. modify and delete modules, lessons and quizes',
+                                                   },
+                                                   {
+                                                     src: '/img/FreeLearnUserHome.jpg',
+                                                     alt: 'home page of a user with enrollments, my content and settings buttons',
+                                                   }
+                                                 ]}
+                                                 summary={`FreeLearn was a learning website much like Khan Academy or Udemy. 
+                                                 FreeLearn was my final year project in university. The challange here was 
+                                                 to use Node.js & Express.js which none of us was familiar with and build a website 
+                                                 from scratch. We've had to design our own database schema, implement token-based authentication & routing.
+                                                 We've used UML throughout the project to convey more complex ideas. The choice of Node meant that the whole codebase was in JavaScript. 
+                                                 This was done in a small, 3-person Scrum group in which we spent a lot of time on discussing the design and layout of every page so that the website provided good user experience.
+                                                 `}
+                                                 language={{
+                                                   name: 'JavaScript',
+                                                   url: 'https://nodejs.org/en/',
+                                                 }}
+                       />}
+                />
+                <Route path='/projects/flasky-blog'
+                       component={() => <Project name="Flasky Blog"
+                                                 methodology="solo project"
+                                                 repo="https://github.com/nl253/FlaskyBlog"
+                                                 summary={`This blog was supposed to store and display my university notes. 
+                                                 It was also  one of the first semi-serious projects I worked on. 
+                                                 I spent about a month working on it on during the summer after the first year of my degree. 
+                                                 In that project I learnt how to use Python and I taught myself basics of Flask. 
+                                                 I ended up not using the blog but it is out there on the internet (deployed on Heroku) available for anyone interested to see.
+                                                 `}
+                                                 date='August 2017'
+                                                 language={{
+                                                   name: 'Python',
+                                                   url: 'https://www.python.org',
+                                                 }}
+                                                 imgs={[
+                                                   {
+                                                     src: '/img/FlaskyBlogHome.jpg',
+                                                     alt: 'home page of a blog with a sidebar listing categories',
+                                                   },
+                                                   {
+                                                     src: '/img/FlaskyBlogCategory.jpg',
+                                                     alt: 'a blog page with a sidebar and an expanded category listing posts',
+                                                   },
+                                                   {
+                                                     src: '/img/FlaskyBlogPost.jpg',
+                                                     alt: 'a blog post',
+                                                   }
+                                                 ]}
+                                                 frameworks={[
+                                                   {
+                                                     name: 'Flask',
+                                                     url: 'http://flask.pocoo.org',
+                                                   },
+                                                   {
+                                                     name: 'Bootstrap',
+                                                     url: 'https://getbootstrap.com/',
+                                                   }]}
+                       />}
+                />
+                <Route path='/projects/sqlite-repl'
+                       component={() => <Project name="SQLite REPL"
+                                                 framework={{
+                                                   name: 'prompt-toolkit',
+                                                   url: 'https://python-prompt-toolkit.readthedocs.io/en/master/index.html',
+                                                 }}
+                                                 summary={`Interactive command line client for accessing and modifying SQLite databases for Linux. 
+                                                 The REPL provides (arguably) better user interface and completion than the REPL sqlite ships with.
+                                                 It features syntax highlighting which a user can alter by changing the theme. 
+                                                 Similarly, a user may tweak the app to display tables differently.
+                                                 `}
+                                                 repo="https://github.com/nl253/SQLiteREPL"
+                                                 methodology="solo project"
+                                                 language={{
+                                                   name: 'Python',
+                                                   url: 'https://www.python.org',
+                                                 }}
+                                                 imgs={[
+                                                   {
+                                                     src: 'https://lh3.googleusercontent.com/IuHm1JDJQDSz8w-WgrhbCGg5Afpp3k1SB48-B1XHVZ0Zj6Lhq-gu8wDP8txR_gB0zPSWTkfUEN_OOhlGi0hOYUN-lYYDdXgiEnOBIgM1EXW9EOTDZrw_lZc1ul6vWfued2oUzy-D0jflaqMgG-3MiiYVTXGoWiu0QzmDpaxF86GkZKITNkPFEjXm4kqowU64qCj4zDtwai6hQrc6w-jpa9zZYW0Gr83ntVZGT_WlBM-DqVF5q63wblw2XgokI4hnM16mqiL7q2xrQ7o0brnNFWPYHUpoRuN3b9wYDuj0Ravz0buxlUzF8QrHTgsmlhooanxbJuB2fDretDUcOBfwErZsSNwsutPVtGXhtBf9FoXg6rx_oGoMMKxZodNR0WnaXxniuHJunxK23x22OAwrRC0NuBSQwKunqt-A_78hiiKU50yIry-80j-S6-WAbis5U1xDiXvBEX_nvTgXDxqiy2mxHPoXIusg3pCUQQhttWW8XZHgMYN3DJzb8Sj_yvtR_y32e_XRLrIJYeREyRoyXNaHnjdtO_ABsvN0Uc74kwOCZ8dbDvNoD8suoAvvcv6pz0R3sCNtTdUuGBRcH266UYQ9dGIDo2tiabyKTOpF42F4PSTopVUppxrDHUy_7iGVIqfoXtHCEPZC5LcHd0BKgXZ1=w1216-h929-no',
+                                                     altText: 'terminal user interface',
+                                                     header: 'Syntax Highlighting',
+                                                   },
+                                                   {
+                                                     src: 'https://lh3.googleusercontent.com/GxBplxLBQdEqqa3eItzUFjuDIh4EmZjCh6q-Ow91tlZbr1U0XXjsNFl0XMMUpfBXAH5gzeYuIe4R-8RQK2IQzKLG4HSk4GhX6HVKZlvgIggWYtZ1Z_RTGTwaKlcqPYGz-nqOZZuYE8IE0yR9bWDUTwz6DLL9TUrx6ay1LGQQUimUtHTWEdbMqykbgA0qyFsDolRzbuNg9WCZqxlBsNIEvo0RpnjhXCEwJHuf05uDUnvUjSSSvi4oo5HvvTSfbejTW0uz-04b40APWolGnmD6Ufcezm_zo4wuSuUfsyfxqYFzjgsIiJnTT13EedQ4kHntZ-14ktitTkGkA6zcKsbsNXc9G-lEIep6AdbjiKqF6u39gYBhg2xloWuYEJsSepG7utiSo_nxI_N27Tjx4Xd-6uEkqCY6oWVL_Y_2EWD-6yxNzMSKoQ8Dh9dBDB4CdE6j5TmDp8TWBgNR83cRF4tHncI6SC0UOoXeQreKRpice2PG5Y8I9jkMGUc0M5Bz2tX12qkLJpcVxQQeb6t7Vlxhpr1-iwd-D0MekovDQgN5yxfwCPoqowXTZYbCkW_MmXly8F1uOEK49QxAKyHfASECXEkMZ1TTIoXKlpTPY-Gl3KJ93ddRdjDWfDWGDuWCbc_qIDOMOnjYTxbeo7mTC_myXG1m=w1497-h927-no',
+                                                     altText: 'terminal user interface',
+                                                     header: 'Syntax Highlighting',
+                                                   },
+                                                   {
+                                                     src: 'https://lh3.googleusercontent.com/D6_e_iIY5kJ6BqKfd7RXTF20UjKN69f-NI6SP6jxDq1rqwGE3mpEvU3W1iMSmyiDqt-6T50eGGSThZiEkDTQdk1PHhOIdI854XMy-34xCvIMgLgRLdKBaYIUeb-2gSddT5eV4PzI5KikEYDalFf95Swyone1whmhrw75-DuGMgLjREoupECLrdwF3LjSTRwvT0Ebvg0oVe3UOKAmK1jfVgp-5_5KJl-EeBMdvsXt4BlD8VenMkkgBmz1GjNYeCXBcj_8sGqs7oTz3NT2o1LhyBsOhD8AgAxJYiCkAkO-rTW2E0tS_1Upskp7eXORovBglQ8tUni9FQM-Z1qYNd31OwDiX7qn9T2nEP1uUf0ZB4tOVfT3TUzKfbucWKWrOj9MJdLb9f44r8uvUzXlrVM2vO36gMu3TByq_yRlL8O8-dnhLxftdi0I-HZFQLsWs2KyhJ2XmEfUislr0WGYQ0NhdIMMYfq3RQxf5AaK_s1IpW8zoyvIY9qPc_qjoVMvLwZSBCcEJIygGxLBtP86NXFiQTyEyF_buxzZz8iR-pusfXZClGdM2OeineuD4zNUc8lMNN_f8LEdaX1mD2-qeYWCiADsFtgQ-cHh9U7bR_SN6zwt6nBddca5VqWt-4gt9DWDKE6AVGzQMgk4dEnmpALDIxza=w645-h391-no',
+                                                     altText: 'terminal user interface',
+                                                     header: 'Syntax Highlighting',
+                                                   },
+                                                   {
+                                                     src: 'https://lh3.googleusercontent.com/ODJddNx0uxsQvKWGMoH5OixlPfY0qkCNBZ9CNTvD0NOF-Z4uc3GnFNZzd2z2Ll-mXpwz-MJF4GvllewlX0n10PFWz2F8saAEpirqqgB8uAvzGL-9Y7qxkR3eCyy7NvV7_4TJO4WtpUmq2_wKIgfJ_Wq9-NHZC6BGrm5FXB_ZckvDFYBKiUjGz1uF4S38Pvc1zIXxtTxkn5QwEgz3w9RFg2DBSJFCIeno5Am_ig2y2F2ZEPd1mPhAubTvz0zt70JT6b6enPKhfJa50tTCvL_Vdhn-c4r7EkG-fWtpuw7neIm5er7k6fc4Q7gfEAIEQkxTM2wszuR4LOtdP8Wu0zuOjntzZa-KCXKWABsInd_4nmwWhDddEL3ozVE9A-gJkaqSqbqNdmCdfpQp320GZl-quYXGoNQDmjoz0yXwYWIacfws0Vmmxg6EIZwZLBuzEe0Mr5paqbxtknZNLlqloZ57AoCY5pPBz0sEclYEKIFVYZH9N2QVMCynvDniLFKqEZ8dlhonS5vL6_KNhOQ5dzXV8xMjpaclA0q1ttjOnAW7YF-qOILjOGepL2jDXpEvyhwjLPX_6KEhTYJ_F880oGHSKuc2K092UTdm160Tnv6ScUq5Ht_amUiNd0QOUEbJZQFr3nCgbh3Ip51ssJbYjJ_UQZ8U=w597-h300-no',
+                                                     altText: 'terminal user interface',
+                                                     header: 'Syntax Highlighting',
+                                                   },
+                                                   {
+                                                     src: 'https://lh3.googleusercontent.com/-fCzGSlc8oIiBa_s9yBc2r2KuJd5GBUdPreOuTnGAIuLO3SX7xs8rjKK9GjyzxijyVzThbymDCYW9XVmFUwpZlstP0_vsSK5M9TXKxs9qbOA6Y0QgzgZ97RevpEGLKgN7LD98Dx_OibHfkWml7qr0jF6TjrNQndAFFKUpIj_o9f67l0LNx1hJoaDOwsCdPUSNrtc-RxXNg-GfUR4kDwT-GS78EdFxZFF77HCgx6Ok5TfbDo_tR-4SwVfzSp7N7_fvzgTSsfJZGhi2c38PGd86z7Q-aTFQnRe4ocG0lMq6I91-kc0L0SBMm6pHP5qiYsvM7kFIx4_U8zBuWYKSWL6_brRNNapTiRG3rLx9SY-Kij8hZBksFrspO_UvK_RBxE6PISq7RhlUokdKkdmbYoKvFcUM49YNF-CJaB8QtYhLjwGcehKNKFkUbkkIWiB61rbl-V-sr4c9dhIM5xF3LqkqPrfsu3xD1iwv3GY6ciWPc6bilJ3WAEJhbOMFWTX9FY_WW8Fsirav9Y2CauC1O9pY7ltns1OmSXlgyEB_LisWyGzqqk-SccMA1ldeuqAWIRz0hbXJaO2N7JUhE1lRHKfC-Ofos1RE8Reot8VhjkFIhLoFqNBwYrpxUs0yieTzceX_3PPo1aKRIJsMK3aWJsb13BU=w1666-h937-no',
+                                                     altText: 'terminal user interface',
+                                                     header: 'Syntax Highlighting',
+                                                   },
+                                                 ]}
+                       />}
+                />
+                <Route path='/projects/regex-engine'
+                       component={() => <Project name="Regex Engine"
+                                                 repo="https://github.com/nl253/RegexEngine"
+                                                 summary={`A small regular expression engine that I built when learning about theory that underlies computation. 
+                                                 Although the program is very simple it does demonstrate how finite automatas are used in implementing regular expression engines. 
+                                                 The program also uses a recursive descent parser for parsing the regular expressions received as input.`}
+                                                 repo="https://github.com/nl253/SQLiteREPL"
+                                                 methodology="solo project"
+                                                 language={{
+                                                   name: 'JavaScript',
+                                                   url: 'https://nodejs.org/en/',
+                                                 }}
+                       />}
+                />
+                <Route path='/projects/virtual-machine'
+                       component={() => <Project name="Virtual Machine"
+                                                 repo="https://github.com/nl253/VirtualMachine"
+                                                 methodology="solo project"
+                                                 language={{
+                                                   name: 'Rust',
+                                                   url: 'https://www.rustlang.org/learn',
+                                                 }}
+                       />}
+                />
+                <Route path='/projects/taxi-cumulus'
+                       component={() => <Project name="Taxi Cumulus"
+                                                 date="October 2017 to May 2018"
+                                                 repo="https://github.com/nl253/Software-Engineering-Project"
+                                                 framework={{
+                                                   name: 'JavaFX',
+                                                   url: 'https://docs.oracle.com/javafx/2/get_started/jfxpub-get_started.htm',
+                                                 }}
+                                                 summary={`This was an Agile university project that was supposed to teach us good software engineering practices. 
+                                                 The app was a graphical user interface (GUI) application built with Java (so cross-platform) that was created for an imaginary company called "Taxi Cumulus".
+                                                 It was meant to help the HR department manage "annual performance review records".
+                                                 We used the standard Agile practices such as user stories, UML, sprint backlog etc.
+                                                 Thought the project we also had to use UML (class & sequence diagrams) to discuss and plan the architecture of the system we were designing.
+                                                 `}
+                                                 methodology="Agile (Scrum)"
+                                                 language={{
+                                                   name: 'Java',
+                                                   url: 'https://docs.oracle.com/en/java/index.html',
+                                                 }}
+                       />}
+                />
+              </div>
+            </div>
+          </div>
+        </Container>
+        <footer/>
+      </div>
+    </Router>,
+    document.getElementById('root'));
+
+registerServiceWorker();
